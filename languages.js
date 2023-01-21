27d228e1f73354138e13a656a3361592abca11f9
Zeh Fernandes <ozehfernandes@gmail.com>
2023-01-21 14:26:59 -0300

    Add new languages

--- languages.js
+++ languages.js
@@ -1,6 +1,9 @@
 import enflag from "./assets/flags/enflag.png";
 import frflag from "./assets/flags/frflag.png";
 import esflag from "./assets/flags/esflag.png";
+import ptflag from "./assets/flags/ptflag.png";
+import itflag from "./assets/flags/itflag.png";
+import deflag from "./assets/flags/deflag.png";
 
 export const languages = [
   {
@@ -21,4 +24,23 @@ export const languages = [
     flag: esflag,
     url: "https://www.wordnote.app/dictionaries/dict_es.db",
   },
+  {
+    label: "Portuguese",
+    code: "pt",
+    flag: ptflag,
+    url: "https://www.wordnote.app/dictionaries/dict_pt.db",
+  },
+  {
+    label: "Italian",
+    code: "it",
+    flag: itflag,
+    url: "https://www.wordnote.app/dictionaries/dict_it.db",
+  },
+  {
+    label: "German",
+    code: "de",
+    flag: deflag,
+    url: "https://www.wordnote.app/dictionaries/dict_de.db",
+  },
 ];
+
